af25afc6fefe0e65c9d4f71bad10ce1d2dc032a7
Matheus Carvalho Oliveira <mco.matheus@gmail.com>
2024-03-18 17:49:09 -0300

    Att 18-03-24
    Trabalhando a navegação entre telas de forma nativa e arrumando responsividade no CSS

--- app2/src/App.js
+++ app2/src/App.js
@@ -1,125 +1,27 @@
-import React,{useState} from 'react' 
-import Nota from './componentes/Nota'
-import Resultado from './componentes/Resultado'
-import Imagem from './componentes/Imagem'
+import React,{useState, useEffect} from 'react' 
+import Home from './componentes/Home'
 import './App.css'
 
 export default function App() {
 
-  // Objeto com os carros
-  const carros = [
-    {ano: '2024', modelo: 'FastBack'},
-    {ano: '2020', modelo: 'Polo'},
-    {ano: '2019', modelo: 'Onix'},
-    {ano: '2019', modelo: 'Fit'}
-  ]
+  const [pagina, setPagina] = useState(0)
 
-  // Usando função map para colocar os carros em forma de lista
-  const listaCarros = carros.map(
-    (elemento, indice)=>
-      <option value={elemento.modelo}>{elemento.modelo} - {elemento.ano}</option>
+  useEffect(
+    ()=>{
+        const res = window.location.href.split("?")
+        setPagina(res[1])
+    }
   )
 
-  // States relacionados aos inputs da tela
-  const [nome, setNome] = useState('')
-  const [carro, setCarro] = useState('FastBack')
-  const [notas, setNotas] = useState({
-    "desempenho":"0,0",
-    "consumo":"0,0",
-    "conforto":"0,0"
-  })
-
-  // Função para manipular o state notas
-  const handleNotas=(e)=>{
-    if(e.target.getAttribute('name')=='desempenho'){
-      setNotas({
-        "desempenho":e.target.value,
-        "consumo":notas.consumo,
-        "conforto":notas.conforto
-      })
-    }else if(e.target.getAttribute('name')=='consumo'){
-      setNotas({
-        "desempenho":notas.desempenho,
-        "consumo":e.target.value,
-        "conforto":notas.conforto
-      })
-    }else if(e.target.getAttribute('name')=='conforto'){
-      setNotas({
-        "desempenho":notas.desempenho,
-        "consumo":notas.consumo,
-        "conforto":e.target.value
-      })
-    }
+  const retornaPagina=()=>{
+    if(pagina == 1) return <><h1>Pagina {pagina}</h1></>
+    else if(pagina == 2) return <><h1>Pagina {pagina}</h1></>
+    else return <Home/>
   }
-
-  // Função para salvar/modificar os dados digitados no localStorage
-  const salvar=(chaves, valores)=>{
-    valores.map(
-      (elemento, indice)=>{
-        if(elemento == ''){
-          alert("O campo *" + chaves[indice] + "* está vazio!")
-          return
-        }
-        localStorage.setItem(chaves[indice], elemento)
-    })
-    let soma = parseFloat(valores[2])+parseFloat(valores[3])+parseFloat(valores[4])
-    localStorage.setItem("soma", soma)
-    localStorage.setItem("status", soma >= 60 ? "aprovado" : "reprovado")
-    alert("Dados salvos!")
-  }
-
-  // Função para consultar os dados que estão salvos no localStorage
-  const consultar=(chaves)=>{
-    chaves.map(
-      (elemento, indice)=>
-      alert(chaves[indice] + " : " + localStorage.getItem(elemento))
-    )
-    alert("Soma das notas : " + localStorage.getItem("soma"))
-    alert("Resultado : " + localStorage.getItem("status"))
-  }
-
-  // Função para apagar os dados que estão armazenados no localStorage
-  const apagar=(chaves)=>{
-    if(window.confirm("Deseja apagar os dados?")){
-      chaves.map(
-        (elemento, indice)=>
-        localStorage.removeItem(elemento)
-      )
-      localStorage.removeItem("soma")
-      localStorage.removeItem("status")
-      alert("Dados apagados!")
-    }
-  }
-
-  // Variável para saber quais os campos do form
-  const camposForm = [
-    "nome",
-    "carro",
-    "notaDesempenho",
-    "notaConsumo",
-    "notaConforto"
-  ]
 
   return (
-    <main className='main'>
-      <h1 className='main__title'>Formulário</h1>
-      <label className='main__label'>Digite seu nome </label>
-      <input type="text" placeholder='digite seu nome' value={nome} onChange={(e)=>{setNome(e.target.value)}}/>
-      <br/>
-      <label className='main__label'>Selecione um carro </label>
-      <select value={carro} onChange={(e)=>{setCarro(e.target.value)}}>
-        {listaCarros}
-      </select>
-      <Imagem carro={carro}/>
-      <Nota atributoAvaliado="desempenho" nota={notas.desempenho} setNotas={handleNotas}/>
-      <Nota atributoAvaliado="consumo" nota={notas.consumo} setNotas={handleNotas}/>
-      <Nota atributoAvaliado="conforto" nota={notas.conforto} setNotas={handleNotas}/>
-      <Resultado somaNotas={parseFloat(notas.desempenho)+parseFloat(notas.consumo)+parseFloat(notas.conforto)}/>
-      <div className='main__botoes'>
-        <button onClick={()=>{salvar(camposForm, [nome, carro, notas.desempenho, notas.conforto, notas.consumo])}} className='main__botoes__salvar'>Salvar</button>
-        <button onClick={()=>{consultar(camposForm)}} className='main__botoes__consultar'>Consultar</button>
-        <button onClick={()=>{apagar(camposForm)}} className='main__botoes__apagar'>Apagar</button>
-      </div>
-    </main>
+    <>
+      {retornaPagina()}
+    </>
   );
 }
---
+++ app2/src/componentes/Menu.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+export default class Menu extends React.Component{
+    // Para usar props
+    constructor(props){
+        super(props)
+
+        // States
+        this.state={
+            pagina: 0
+        }
+
+        // Outras instruções do construtor
+    }
+
+    // Manipular state
+    trocarPagina(p){
+        if(p == 1) window.open("http://localhost:3000?1", "_self")
+        else if(p == 2) window.open("http://localhost:3000?2", "_self")
+        else window.open("http://localhost:3000", "_self")
+    }
+
+    componentDidMount(){
+        console.log('Componente criado!')
+    }
+
+    componentDidUpdate(){
+        console.log('Componente atualizado!')
+    }
+
+    componentWillUnmount(){
+        console.log('Componente removido!')
+    }
+
+    render(){
+        return(
+            <nav className='main__menu'>
+                <a onClick={()=>this.trocarPagina(0)} className="main__menu__link">Home</a>
+                <a onClick={()=>this.trocarPagina(1)} className="main__menu__link">Página 1</a>
+                <a onClick={()=>this.trocarPagina(2)} className="main__menu__link">Página 2</a>
+            </nav>
+        )
+    }
+        
+}
