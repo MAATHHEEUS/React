37053ec10c6dabd3ca76b3ed85c677af0c4e2870
Matheus Carvalho Oliveira <mco.matheus@gmail.com>
2024-03-22 15:07:20 -0300

    Att 22-03-24
    Atualizando a calculadora, criando os botões e regras de preenchimento

--- app2/src/componentes/CalcIMC.js
+++ app2/src/componentes/CalcIMC.js
@@ -40,7 +40,7 @@ const inputPeso=(p, sp) => {
     return(
         <div>
             <label className='main__label'>Peso </label>
-            <input className='main__input' type="text" placeholder='digite seu peso em kg' value={p} onChange={(e)=>{sp(e.target.value)}}/>
+            <input id='input_peso' className='main__input' type="text" readOnly placeholder='digite seu peso em kg' value={p} onChange={(e)=>{sp(e.target.value)}}/>
         </div>
     )
 }
@@ -49,18 +49,20 @@ const inputPeso=(p, sp) => {
 const inputAltura=(a, sa) => {
     return(
         <div>
-            <label className='main__label__calcIMC'>Altura </label>
-            <input className='main__input' type="text" placeholder='digite sua altura em metros' value={a} onChange={(e)=>{sa(e.target.value)}}/>
+            <label className='main__label'>Altura </label>
+            <input id='input_altura' className='main__input' type="text" readOnly placeholder='digite sua altura em metros' value={a} onChange={(e)=>{sa(e.target.value)}}/>
         </div>
     )
 }
 
 // Função para calcular o resultado e retorna o botão
-const calcular=(p, a, sr) => {
+const calcular=(sr) => {
     const calcularIMC=()=>{
-        if(isNaN(p))alert('Peso deve ser apenas números separados por ponto(.). Ex.:75.8')
-        if(isNaN(a))alert('Altura deve ser apenas números separados por ponto(.). Ex.:1.75')
-        sr(p/(a * a))
+        let peso = document.getElementById('input_peso').value
+        let altura = document.getElementById('input_altura').value
+        if(isNaN(peso) || peso == '')alert('Peso deve ser apenas números separados por ponto. Exemplo: 75.8')
+        if(isNaN(altura) || altura == '')alert('Altura deve ser apenas números separados por ponto. Exemplo: 1.75')
+        sr(peso/(altura * altura))
     }
     return(
         <button className='main__botaoCalcular' onClick={calcularIMC}>Calcular</button>
@@ -74,6 +76,54 @@ const mostraResultado=(r) => {
     )
 }
 
+// Função para criar um botão da calculadora
+const botaoCalculadora=(conteudo) => {
+    const print=(conteudo)=>{
+        // Descobre qual input está destacado
+        let inputDestacado = ''
+        const inputs = document.getElementsByClassName("main__input");
+        for (let i = 0; i < inputs.length; i++) {
+            if(inputs[i].style.borderColor == "green") inputDestacado = inputs[i].id
+        }
+        if(inputDestacado != ''){
+            // Troca a vírgula por ponto
+            if(conteudo == ',')conteudo = '.'
+            // Não permite digitar mais de um ponto
+            if(!String(document.getElementById(inputDestacado).value).includes('.') || conteudo != '.'){
+                document.getElementById(inputDestacado).value += conteudo
+            }
+        }
+    }
+    return(
+        <button className='main__botaoCalculadora' onClick={()=>print(conteudo)}>{conteudo}</button>
+    )
+}
+
+const botaoSelecionarInput=(input)=> {
+    const mudarCor=(input)=>{
+        const inputs = document.getElementsByClassName("main__input");
+        for (let i = 0; i < inputs.length; i++) {
+            inputs[i].style.borderColor = "transparent"
+        }
+        document.getElementById('input_'+input).style.borderColor = 'green'
+    }
+    return(
+        <button className='main__botaoCalculadora' onClick={()=>mudarCor(input)}>{input}</button>
+    )
+}
+
+const botaoApagarInput=() => {
+    const apagar=()=>{
+        const inputs = document.getElementsByClassName("main__input");
+        for (let i = 0; i < inputs.length; i++) {
+            inputs[i].value = ""
+        }
+    }
+    return(
+        <button className='main__botaoApagar' onClick={()=>apagar()}>apagar</button>
+    )
+}
+
 export default function CalcIMC(){
 
     const [peso, setPeso] = useState('')
@@ -84,11 +134,21 @@ export default function CalcIMC(){
         <main className='main'>
             <h1 className='main__title'>Calculadora de IMC</h1>
             <Menu/>
-            {inputPeso(peso, setPeso)}
-            {inputAltura(altura, setAltura)}
-            {calcular(peso, altura, setResultado)}
-            {mostraResultado(resultado)}
-            {tabela(resultado)}
+            <div className='main__conteudo'>
+                <div className='main__conteudo__calculadora'>
+                    <div className='div_botoesCalculadora'>{botaoCalculadora(1)}{botaoCalculadora(2)}{botaoCalculadora(3)}</div>
+                    <div className='div_botoesCalculadora'>{botaoCalculadora(4)}{botaoCalculadora(5)}{botaoCalculadora(6)}</div>
+                    <div className='div_botoesCalculadora'>{botaoCalculadora(7)}{botaoCalculadora(8)}{botaoCalculadora(9)}</div>
+                    <div>{botaoSelecionarInput('peso')}{botaoCalculadora(0)}{botaoSelecionarInput('altura')}</div>
+                    <div>{botaoApagarInput()}{botaoCalculadora(',')}{calcular(setResultado)}</div>
+                </div>
+                <div className='main__conteudo__resultado'>  
+                    {inputPeso(peso, setPeso)}
+                    {inputAltura(altura, setAltura)}
+                    {mostraResultado(resultado)}
+                    {tabela(resultado)}
+                </div>
+            </div>
         </main>   
     )
 }
