7e43f03ab2ed535f1e037cdeeb0a1fe64754b20d
Matheus Carvalho Oliveira <mco.matheus@gmail.com>
2024-03-27 17:37:13 -0300

    Att 27-03-24
    Criando calculadora simples, criando restrições de digitação!

--- app03/src/App.js
+++ app03/src/App.js
@@ -1,24 +1,156 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// Elementos
+const Tela=(valor, res)=> {
+  return(
+    <div style={cssTela}>
+      <span style={cssTelaOper}>{valor}</span>
+      <span style={cssTelaRes}>{res}</span>
     </div>
+  );
+} 
+
+const btn=(label, func)=> {
+  return(
+    <button style={cssBtn} onClick={func}>{label}</button>
+  );
+}
+
+// Estilos
+const cssTela={
+  display: 'flex',
+  paddindLeft: 20,
+  paddindRight: 20,
+  justifyContent: 'center',
+  alignItems: 'flex-start',
+  flexDirection: 'column',
+  backgroundColor: '#444',
+  width: 300
+}
+
+const cssTelaOper={
+  fontSize: 25,
+  color: '#fff',
+  height: 20
+}
+
+const cssTelaRes={
+  fontSize: 50,
+  color: '#fff',
+}
+
+const cssBtn={
+  fontSize: 30,
+  color: '#fff',
+  height: 75,
+  width: 75,
+  padding: 20,
+  backgroundColor: '#000',
+  color: '#fff',
+  textAlign: 'center',
+  border: 'none'
+}
+
+const cssContainer={
+  display: 'flex',
+  justifyContent: 'flex-start',
+  alignItems: 'center',
+  flexDirection: 'column',
+  width: 300,
+  border: '1px solid #000'
+}
+
+const cssBotoes={
+  flexDirection: 'row',
+  flexWrap: 'wrap'
+}
+
+function App() {
+  
+  const [valorTela, setValorTela] = useState('');
+  const [resultado, setResultado] = useState(0);
+  const [acumulador, setAcumulador] = useState(0);
+  const [operado, setOperado] = useState(false);
+
+  // Funções
+  const addDigitoTela=(digito)=> {
+    // Só digita ponto na tela se não houver nenhum 
+    if((digito == '+' || digito == '-' || digito == '*' || digito == '/' || digito == '.') && String(valorTela).endsWith(digito)){
+      return;
+    }else{
+      if((digito == '+' || digito == '-' || digito == '*' || digito == '/') && operado){
+        setOperado(false);
+        setValorTela(resultado+digito);
+        return;
+      }else if(operado){
+        setValorTela(digito);
+        setOperado(false);
+        return;
+      }else{
+        const valorDigitadoTela = valorTela+digito;
+        setValorTela(valorDigitadoTela);
+      }
+    }
+  }
+
+  const limparMemoria=()=> {
+    setOperado(false);
+    setResultado(0);
+    setAcumulador(0);
+    setValorTela('');
+    return;
+  }
+
+  const Operacao=(oper)=> {
+    if(oper == 'backSpace'){
+      let vTela = valorTela;
+      vTela = vTela.substring(0, vTela.length-1);
+      setValorTela(vTela);
+      setOperado(false);
+      return;
+    }
+    try{
+      // Eval retorna o resultado da expressão passada - Cálculo
+      const r = eval(valorTela)
+      setAcumulador(r);
+      setResultado(r);
+      setOperado(true);
+    }
+    catch{
+      setResultado('#ERRO');
+    }
+  }
+  
+  return (
+    <>
+      <div style={cssContainer}>
+        <h3>Calculadora Simples</h3>
+        {Tela(valorTela, resultado)}
+        <div style={cssBotoes}>
+          {btn('AC', limparMemoria)}
+          {btn('(', ()=>addDigitoTela('('))}
+          {btn(')', ()=>addDigitoTela(')'))}
+          {btn('/', ()=>addDigitoTela(')'))}
+          {btn('7', ()=>addDigitoTela('7'))}
+          {btn('8', ()=>addDigitoTela('8'))}
+          {btn('9', ()=>addDigitoTela('9'))}
+          {btn('*', ()=>addDigitoTela('*'))}
+          {btn('4', ()=>addDigitoTela('4'))}
+          {btn('5', ()=>addDigitoTela('5'))}
+          {btn('6', ()=>addDigitoTela('6'))}
+          {btn('-', ()=>addDigitoTela('-'))}
+          {btn('1', ()=>addDigitoTela('1'))}
+          {btn('2', ()=>addDigitoTela('2'))}
+          {btn('3', ()=>addDigitoTela('3'))}
+          {btn('+', ()=>addDigitoTela('+'))}
+          {btn('0', ()=>addDigitoTela('0'))}
+          {btn('.', ()=>addDigitoTela('.'))}
+          {btn('<-', ()=>Operacao('backSpace'))}
+          {btn('=', ()=>Operacao('='))}
+        </div>
+      </div>
+    </>
   );
 }
 
